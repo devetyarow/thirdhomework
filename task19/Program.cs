1fd554d28b88f836a59d0c7896fe46312b7f5b62
Artem <devetyarow@mail.ru>
2022-11-29 21:25:23 +0300

    вроде все работает

---
+++ task19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+// вспомнить че такое палиндром
+
+Console.Write("Enter five-digit number: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Your number: {number} - palindrome.");
+  }
+  else Console.WriteLine($"Your number: {number} - Not palindrome.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Enter five-digit number");
---
+++ task23/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи число и узри кубы этих чисел!:");
+            int N = int.Parse(Console.ReadLine());
+ 
+            Console.WriteLine("Числицо" + "\t" + "Кубец");
+            for(int i = 1; i <= N; i++)
+                Console.WriteLine(i + "\t" + i*i*i);
+ 
+            Console.ReadLine();
+  
+        }
+    }
